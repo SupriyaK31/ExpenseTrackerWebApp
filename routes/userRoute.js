7e2094253959495924efe446dce6426031dae314
SupriyaK31 <supriyakittur31@gmail.com>
2024-02-15 17:25:07 +0530

    unique email

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,9 +2,13 @@ const express=require('express');
 const routes=express.Router();
 const path=require('path');
 const User=require('../models/user');
+const { Sequelize } = require('sequelize');
 
 routes.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'../','views','Signup.html'));
+})
+routes.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../','views','login.html'));
 })
 routes.post('/Signup',(req,res)=>{
     const name = req.body.name;
@@ -17,7 +21,17 @@ routes.post('/Signup',(req,res)=>{
     }).then((result)=>{
         console.log(result);
         console.log('Signup Successful');
-    }).catch((err)=>{console.log(err)})
+        // res.status(201).json(result);
+        res.redirect('/login');
+    }).catch((err)=>{
+        if(err instanceof Sequelize.UniqueConstraintError){
+            res.status(400).json({ err: 'Email already exists' });
+        }else{
+            res.status(500).json({ err: 'Internal Server Error' });
+        }
+    })
 });
+
+
 
 module.exports=routes;
