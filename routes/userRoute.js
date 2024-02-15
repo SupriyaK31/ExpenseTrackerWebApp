70f6ef6203ce00afd891878fc992cfe3b4b84ea5
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-02-15 20:57:01 +0530

    change

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,5 +7,5 @@ const userController=require('../controllers/userController');
 routes.get('/',userController.getIndex);
 routes.get('/login',userController.getLogin);
 routes.post('/Signup',userController.postSignup);
-
+routes.post('/Login',userController.postLogin);
 module.exports=routes;
