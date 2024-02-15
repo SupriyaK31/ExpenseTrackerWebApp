21594af5617354f9450eaf27a7cab443f92fb2fc
SupriyaK31 <supriyakittur31@gmail.com>
2024-02-15 18:05:35 +0530

    changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,37 +1,11 @@
 const express=require('express');
 const routes=express.Router();
-const path=require('path');
-const User=require('../models/user');
-const { Sequelize } = require('sequelize');
 
-routes.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../','views','Signup.html'));
-})
-routes.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../','views','login.html'));
-})
-routes.post('/Signup',(req,res)=>{
-    const name = req.body.name;
-    const email = req.body.email;
-    const password=req.body.password;
-    const user=User.create({
-        name:name,
-        email:email,
-        password:password
-    }).then((result)=>{
-        console.log(result);
-        console.log('Signup Successful');
-        // res.status(201).json(result);
-        res.redirect('/login');
-    }).catch((err)=>{
-        if(err instanceof Sequelize.UniqueConstraintError){
-            res.status(400).json({ err: 'Email already exists' });
-        }else{
-            res.status(500).json({ err: 'Internal Server Error' });
-        }
-    })
-});
+const userController=require('../controllers/userController');
 
 
+routes.get('/',userController.getIndex);
+routes.get('/login',userController.getLogin);
+routes.post('/Signup',userController.postSignup);
 
 module.exports=routes;
