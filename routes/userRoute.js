4d72956e002c1a2d2888a1b7614b78b2dde41629
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-02-16 15:53:40 +0530

    reoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,10 +2,13 @@ const express=require('express');
 const routes=express.Router();
 
 const userController=require('../controllers/userController');
-
+const expenseController=require('../controllers/expenseController');
 
 routes.get('/',userController.getIndex);
 routes.get('/login',userController.getLogin);
 routes.post('/Signup',userController.postSignup);
 routes.post('/Login',userController.postLogin);
+routes.get('/expense',expenseController.expensePage);
+routes.post('/addExpense',expenseController.addExpense);
+routes.get('/showExpense',expenseController.getExpenseList);
 module.exports=routes;
