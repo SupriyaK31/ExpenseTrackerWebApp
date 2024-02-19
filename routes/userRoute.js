f89c92bd07ee26e3e5f83aa32cf786c70d54f195
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-02-19 18:49:36 +0530

    tokenroute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const routes=express.Router();
 
 const userController=require('../controllers/userController');
 const expenseController=require('../controllers/expenseController');
+const userAuth=require('../middleware/auth');
 
 routes.get('/',userController.getIndex);
 routes.get('/login',userController.getLogin);
@@ -10,5 +11,6 @@ routes.post('/Signup',userController.postSignup);
 routes.post('/Login',userController.postLogin);
 routes.get('/expense',expenseController.expensePage);
 routes.post('/addExpense',expenseController.addExpense);
-routes.get('/showExpense',expenseController.getExpenseList);
+routes.get('/showExpense',userAuth.authonticate,expenseController.getExpenseList);
+routes.delete('/delExpense/:id',expenseController.delExpense);
 module.exports=routes;
