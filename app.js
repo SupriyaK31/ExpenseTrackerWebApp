e39436645c523fd1d74c02180b448dfb63266276
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-02-19 18:46:54 +0530

    token

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express=require('express');
 const sequelize=require('./utils/database');
-const userRoutes=require('./routes/route');
+const userRoutes=require('./routes/userRoute');
 const cors = require('cors');
 const app=express();
 const PORT=3000;
+const User=require('./models/user');
+const Expense=require('./models/expenseModel');
 app.use(cors());
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(userRoutes);
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
 sequelize.sync().then(()=>{
     app.listen(PORT,()=>{ 
