71638214d853843662106788456eb68f0e674133
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-02-21 18:20:56 +0530

    app order

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express=require('express');
 const sequelize=require('./utils/database');
 const userRoutes=require('./routes/userRoute');
+const purchaseRoutes=require('./routes/purchaseRoutes');
 const cors = require('cors');
 const app=express();
 const PORT=3000;
 const User=require('./models/user');
 const Expense=require('./models/expenseModel');
+const Order = require('./models/order');
 app.use(cors());
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(userRoutes);
+app.use(purchaseRoutes);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize.sync().then(()=>{
     app.listen(PORT,()=>{ 
