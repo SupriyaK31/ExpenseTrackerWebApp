5618d6228fa403946f98c1d389f9b75bf5a0ef33
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-02-26 16:23:20 +0530

    transaction

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -3,6 +3,7 @@ const Expense=require('../models/expenseModel');
 const {Sequelize}=require('sequelize');
 const path=require('path');
 const bcrypt=require('bcrypt');
+const sequelize = require('../utils/database');
 
 
 const expensePage=(req,res)=>{
@@ -18,25 +19,31 @@ function isstringValidator(string){
 
 const addExpense= async(req,res)=>{
     try{
+        const t=await sequelize.transaction();
         const Amount = req.body.Amount;
         const userId=req.user.id;
         // console.log("req.body"+userId);
         const description = req.body.description;
         const category=req.body.category;
-        Expense.create({Amount,description,category,userId}).then((expense)=>{
+        await Expense.create({Amount,description,category,userId},{transaction:t}).then(async(expense)=>{
             // console.log("total expense"+Number(req.user.totalExpenses));
         const totalExpense=Number(req.user.totalExpenses)+Number(Amount);
         console.log(totalExpense);
-        User.update({
+        await User.update({
             totalExpenses:totalExpense
-        },{where:{id:req.user.id}
+        },{
+            where:{id:req.user.id},
+            transaction:t
        }).then(async()=>{
+        await t.commit();
         res.status(200).json({expense:expense});
        }) 
-       }).catch((err)=>{
+       }).catch(async(err)=>{
+         await t.rollback();
         res.status(402).json('Not Found');
        })
     }catch(error){
+     await t.rollback();
         console.error(error);
         res.status(500).json('internal server issue');
     }
@@ -54,28 +61,37 @@ const getExpenseList= async(req,res)=>{
 
 }
 
-const delExpense=async(req,res)=>{
-    const expenseId = req.params.id;
-    // console.log('expenseID:'+ expenseId);
-    try{
-        if(expenseId== undefined || expenseId.length===0){
-            return res.status(400).json({error:'Expense not Found'});
+const delExpense = async (req, res) => {
+    const expenseId=req.params.id;
+    try {
+        const t=await sequelize.transaction();
+        const expense = await Expense.findOne({ where: { id: expenseId, userId: req.user.id } });
+        if (!expense) {
+            return res.status(404).json({ error: 'Expense not Found' });
         }
-        Expense.destroy({where:{id:expenseId,userId:req.user.id}}).then((noofrows)=>{
-            if(noofrows===0){
-                return res.status(404).json({error:'Expense not Found'});
-            }
-            const totalExpense=Number(req.user.totalExpenses)-Number(Amount);
-            User.update({
-                totalExpenses:totalExpense
-            },{where:{id:req.user.id} })
-            res.status(201).json({message:'Expense deleted sucessfully'});
-              })
-        }
-    catch(error){
-        res.status(500).json({error:'Internal Server Error'});
-    }  
+        const totalExpense = Number(req.user.totalExpenses) - Number(expense.Amount);
+        
+        await Expense.destroy({ where: { id: expenseId, userId: req.user.id } },{transaction:t}).then(async()=>{
+        
+        await User.update({ totalExpenses: totalExpense }, 
+            { 
+                where: { id: req.user.id },
+                transaction:t
+            }).then(async()=>{
+                await t.commit();
+                res.status(201).json({ message: 'Expense deleted successfully' });
+            }).catch(async(error)=>{
+                await t.rollback();
+                res.status(404).json({ error: 'not found' });
+            })
+        })
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
+
+
 module.exports={
     expensePage,
     addExpense,
