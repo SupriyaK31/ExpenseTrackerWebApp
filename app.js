cfd1013723684a914f78f707a81fc219e31cbda5
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-02-29 18:03:32 +0530

    app.js

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 const express=require('express');
+const cors = require('cors');
+
 const sequelize=require('./utils/database');
 const userRoutes=require('./routes/userRoute');
 const purchaseRoutes=require('./routes/purchaseRoutes');
 const expenseRoutes=require('./routes/expenseRoute');
-const cors = require('cors');
-const app=express();
-const PORT=3000;
+const forgetPassRoutes=require('./routes/forgetpassword');
+
 const User=require('./models/user');
 const Expense=require('./models/expenseModel');
 const Order = require('./models/order');
-app.use(cors());
+const Forgotpassword=require('./models/forgetPassword');
 
+const app=express();
+const PORT=3000;
+const dotenv=require('dotenv');
+dotenv.config();
+
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(userRoutes);
 app.use(purchaseRoutes);
 app.use(expenseRoutes);
+app.use(forgetPassRoutes);
+
 User.hasMany(Expense);
 Expense.belongsTo(User);
 
 User.hasMany(Order);
 Order.belongsTo(User);
+
+User.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(User);
 
 sequelize.sync().then(()=>{
     app.listen(PORT,()=>{ 
