1cb4434579a95c2e04125c6d9116b5761a3a3b3a
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-03-06 22:05:46 +0530

    pagination controller

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -4,11 +4,12 @@ const {Sequelize}=require('sequelize');
 const path=require('path');
 const bcrypt=require('bcrypt');
 const sequelize = require('../utils/database');
-
-
+const AWS=require('aws-sdk');
+const File=require('../models/expenseFile');
 const expensePage=(req,res)=>{
  res.sendFile(path.join(__dirname,'../','views','index.html'));
 };
+
 function isstringValidator(string){
     if(string =='undefine' || string.length===0){
         return true;
@@ -48,19 +49,37 @@ const addExpense= async(req,res)=>{
         res.status(500).json('internal server issue');
     }
 };
-const getExpenseList= async(req,res)=>{
-    try{
-        await Expense.findAll({ where: {userId: req.user.id}}).then(expenses=>{
-            return res.status(200).json({expenses,sucess:true});
-        }).catch(err=>{console.log(err);})
-        // console.log(expenses);
+const getExpenseList = async (req, res) => {
+    try {
+      const page = req.query.page || 1; // Default to page 1 if not provided
+      const pageSize = 5; // Number of records per page
+  
+      const offset = (page - 1) * pageSize;
+  
+      const result = await Expense.findAndCountAll({
+        where: { userId: req.user.id },
+        offset: offset,
+        limit: pageSize,
+      });
+  
+      const expenses = result.rows;
+      const totalCount = result.count;
+  
+      const totalPages = Math.ceil(totalCount / pageSize);
+  
+      return res.status(200).json({
+        expenses,
+        totalCount,
+        totalPages,
+        currentPage: page,
+        success: true,
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-
-}
-
+  };
+  
 const delExpense = async (req, res) => {
     const expenseId=req.params.id;
     try {
@@ -91,10 +110,111 @@ const delExpense = async (req, res) => {
     }
 };
 
+function uploadToS3(data,filename){
+    const BUCKET_NAME='supriyaexpensetracker';
+    const IAM_USER_KEY='AKIAZI2LDPW3ABQQ33RL';
+    const IAM_USER_SECRET='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';
+
+    let s3bucket=new AWS.S3({
+        accessKeyId:IAM_USER_KEY,
+        secretAccessKey:IAM_USER_SECRET,
+        // Bucket:BUCKET_NAME
+    })
+ 
+        var params={
+            Bucket:BUCKET_NAME,
+            Key:filename,
+            Body: data,
+            ACL:'public-read'      //Access control level
+        }
+        return new Promise((resolve,reject)=>{
+            s3bucket.upload(params,(err,s3response)=>{
+                if(err){
+                    console.log('something went wrong',err);
+                    reject(err);
+                }else{
+                    console.log('Success',s3response);
+                    resolve(s3response.Location);
+                }
+            })
+        });
+
+}
+const downloadExpenses =  async (req, res) => {
+
+    try {
+        // if(!req.user.ispremiumuser){
+        //     return res.status(401).json({ success: false, message: 'User is not a premium User'})
+        // }
+        const expenses=await req.user.getExpenses();
+       
+        const stringifyExpenses=JSON.stringify(expenses);
+        
+        const userId=req.user.id;
+        const filename=`Expense${userId}/${new Date()}.txt`;
+        const fileURL= await uploadToS3(stringifyExpenses,filename);
+        console.log('file url',fileURL);
+        File.create({fileURL,userId});
+        res.status(200).json({fileURL,success:true});
+        // const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING; // check this in the task. I have put mine. Never push it to github.
+        // // Create the BlobServiceClient object which will be used to create a container client
+        // const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+
+        // // V.V.V.Imp - Guys Create a unique name for the container
+        // // Name them your "mailidexpensetracker" as there are other people also using the same storage
+
+        // const containerName = 'prasadyash549yahooexpensetracker'; //this needs to be unique name
+
+        // console.log('\nCreating container...');
+        // console.log('\t', containerName);
+
+        // // Get a reference to a container
+        // const containerClient = await blobServiceClient.getContainerClient(containerName);
+
+        // //check whether the container already exists or not
+        // if(!containerClient.exists()){
+        //     // Create the container if the container doesnt exist
+        //     const createContainerResponse = await containerClient.create({ access: 'container'});
+        //     console.log("Container was created successfully. requestId: ", createContainerResponse.requestId);
+        // }
+        // // Create a unique name for the blob
+        // const blobName = 'expenses' + uuidv1() + '.txt';
+
+        // // Get a block blob client
+        // const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+        // console.log('\nUploading to Azure storage as blob:\n\t', blobName);
+
+        // // Upload data to the blob as a string
+        // const data =  JSON.stringify(await req.user.getExpenses());
+
+        // const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+        // console.log("Blob was uploaded successfully. requestId: ", JSON.stringify(uploadBlobResponse));
+
+        // //We send the fileUrl so that the in the frontend we can do a click on this url and download the file
+        // const fileUrl = `https://demostoragesharpener.blob.core.windows.net/${containerName}/${blobName}`;
+        //  res.status(201).json({message: expense}); // Set disposition and send it.
+    } catch(err) {
+        res.status(500).json({ error: err, success: false, message: 'Something went wrong'})
+    }
+
+};
+const downloadedFile= (req,res)=>{
+    try{
+        const filelist=File.findAll({where : { id:req.user.id}});
+        console.log(filelist);
+
+    }catch(err){
+        console.log(err);
+    }
+}
 
 module.exports={
     expensePage,
     addExpense,
     getExpenseList,
-    delExpense
+    delExpense,
+    downloadExpenses,
+    downloadedFile
+
 }
